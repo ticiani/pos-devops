452befab450d7e40d451bbbafb47f94a090130d2
ticiani <ticiani@gmail.com>
2019-06-19 22:19:51 -0300

    hello world

--- hello-world.py
+++ hello-world.py
@@ -4,14 +4,16 @@ em um range de linhas '''
 
 x = 2
 
+#para gerar um if, só precisa de : e identação na próxima linha
 if x < 1:
     print("Olá, mundo")
 else:
     print("Hello World")
 
+
+# como fazer um for
 for i in range (10):
     print(i)
-
 
 
 nome = "Ticiani"
@@ -19,3 +21,28 @@ nome = "Ticiani"
 for i in nome:
     print(i)
 
+
+# como fazer uma função 
+
+def somar():
+    print(2+2)
+
+somar()
+
+#o método da função fica dentro do parênteses
+
+def somar(x,y):
+    print(x+y)
+
+somar(3,2)
+
+# inserindo variável na função
+# int é para transformar em número inteiro
+
+def somar(x,y):
+    return(x+y)
+
+n1 = int (input ("Digite o número 1: "))
+n2 = int (input ("Digite o número 2: "))
+
+print (somar(n1,n2))
