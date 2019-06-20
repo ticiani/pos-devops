3d8c5c30ed7645f8e8f855d39942815a48dc3d5f
ticiani <ticiani@gmail.com>
2019-06-19 22:10:43 -0300

    hello world em python

---
+++ hello-world.py
@@ -0,0 +1,21 @@
+#comenta no python
+'''para comentar 
+em um range de linhas '''
+
+x = 2
+
+if x < 1:
+    print("Olá, mundo")
+else:
+    print("Hello World")
+
+for i in range (10):
+    print(i)
+
+
+
+nome = "Ticiani"
+
+for i in nome:
+    print(i)
+
